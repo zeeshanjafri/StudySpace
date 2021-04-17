2030caaf6354d2dc02f8be8c1a9a2156adb0736b
Matthew Francis <mattfrancis888@gmail.com>
2021-04-17 19:24:30 -0400

    Fixed create a post for group page.

--- src/components/Group.js
+++ src/components/Group.js
@@ -17,94 +17,107 @@ const posts = demoPosts.slice(0, 3);
 const groups = demoGroups;
 
 function createPost() {
-  console.log("Post created!");
+    console.log("Post created!");
 }
 
 const Group = (props) => {
-  const pathName = props.location.pathname;
-  const groupSlug = pathName.slice(7);
-  // console.log(groupSlug); // this chooses the slug after '/group/' i.e financial-markets etc.
+    const pathName = props.location.pathname;
+    const groupSlug = pathName.slice(7);
+    // console.log(groupSlug); // this chooses the slug after '/group/' i.e financial-markets etc.
 
-  useEffect(() => {
-    props.fetchGroups();
-    props.fetchPosts();
-  }, []);
+    useEffect(() => {
+        props.fetchGroups();
+        props.fetchPosts();
+    }, []);
 
-  const onCreateAPost = async (formValues) => {
-    const dataProperties = {
-      group_id: "http://studyspace-backend.herokuapp.com/Groups/1/",
-      author: "https://studyspace-backend.herokuapp.com/Users/1/",
-      upvotes: 0,
-      slug: "financial-markets",
+    const renderContent = () => {
+        if (props.groups && props.posts) {
+            // console.log("the prop is a", typeof(props.groups[0].id));
+            // console.log("but the slug is a ",typeof(groupSlug))
+
+            function slugMatches(group) {
+                return group.slug === props.match.params.groupName;
+            }
+
+            const mainGroup = props.groups.find(slugMatches);
+
+            const onCreateAPost = async (formValues) => {
+                const dataProperties = {
+                    group_id: `http://studyspace-backend.herokuapp.com/Groups/${mainGroup.id}/`,
+                    author: "https://studyspace-backend.herokuapp.com/Users/1/",
+                    upvotes: 0,
+                    slug: `${mainGroup.slug}`,
+                };
+                const newObj = { ...formValues, ...dataProperties };
+                console.log(newObj);
+                await props.createAPost(newObj);
+                props.fetchGroups();
+            };
+            // console.log(mainGroup);
+
+            return (
+                <div className="flex flex-col w-full">
+                    <GroupHeader
+                        title={mainGroup.groupName}
+                        tags={mainGroup.tags}
+                        image={mainGroup.picture}
+                    />
+                    <div className="groupDescAndPostContainer space-x-4 flex-row">
+                        <div className="flex flex-col space-y-6 w-1/2">
+                            <GroupDesc
+                                title={mainGroup.groupName}
+                                description={mainGroup.description}
+                            />
+
+                            {/* NewPost Form */}
+                            <div className="border rounded-lg w-full p-8">
+                                <h1 className="mb-4 text-md font-bold">
+                                    Create a New Post
+                                </h1>
+                                <NewPostForm
+                                    onSubmit={(formValues) =>
+                                        onCreateAPost(formValues)
+                                    }
+                                />
+                            </div>
+                        </div>
+
+                        {/* The new post container: */}
+                        <div className="flex flex-col border w-1/2 rounded-lg p-4 bg-gray-50">
+                            <NewPostHeader />
+
+                            <div className="flex flex-col mt-3 space-y-4">
+                                {props.posts.map((post, index) => {
+                                    if (
+                                        props.match.params.groupName ===
+                                        post.slug
+                                    )
+                                        return (
+                                            <PostView key={index} post={post} />
+                                        );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        } else {
+            return <h1>Loading...</h1>;
+        }
     };
-    const newObj = { ...formValues, ...dataProperties };
-    console.log(newObj);
-    await props.createAPost(newObj);
-  };
 
-  const renderContent = () => {
-    if (props.groups && props.posts) {
-      // console.log("the prop is a", typeof(props.groups[0].id));
-      // console.log("but the slug is a ",typeof(groupSlug))
-
-      function slugMatches(group) {
-        return group.slug === props.match.params.groupName;
-      }
-
-      const mainGroup = props.groups.find(slugMatches);
-
-      // console.log(mainGroup);
-
-      return (
-        <div className="flex flex-col w-full">
-          <GroupHeader title={mainGroup.groupName} tags={mainGroup.tags} image={mainGroup.picture}/>
-          <div className="groupDescAndPostContainer space-x-4 flex-row">
-            <div className="flex flex-col space-y-6 w-1/2">
-              <GroupDesc
-                title={mainGroup.groupName}
-                description={mainGroup.description}
-              />
-
-              {/* NewPost Form */}
-              <div className="border rounded-lg w-full p-8">
-                <h1 className="mb-4 text-md font-bold">Create a New Post</h1>
-                <NewPostForm
-                  onSubmit={(formValues) => onCreateAPost(formValues)}
-                />
-              </div>
-            </div>
-
-            {/* The new post container: */}
-            <div className="flex flex-col border w-1/2 rounded-lg p-4 bg-gray-50">
-              <NewPostHeader />
-
-              <div className="flex flex-col mt-3 space-y-4">
-                {props.posts.map((post, index) => {
-                  if (props.match.params.groupName === post.slug)
-                    return <PostView key={index} post={post} />;
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return <h1>Loading...</h1>;
-    }
-  };
-
-  return <React.Fragment>{renderContent()}</React.Fragment>;
+    return <React.Fragment>{renderContent()}</React.Fragment>;
 };
 
 // export default Group;
 const mapStateToProps = (state) => {
-  return {
-    groups: state.groups.data,
-    posts: state.posts.data,
-  };
+    return {
+        groups: state.groups.data,
+        posts: state.posts.data,
+    };
 };
 export default connect(mapStateToProps, {
-  fetchGroups,
-  fetchPosts,
-  createAPost,
+    fetchGroups,
+    fetchPosts,
+    createAPost,
 })(Group);
